a9c5157e5f7a10653e7b17b3906f29367ac8d530
your name <phamlong12082001@gmail.com>
2022-08-03 21:54:56 +0700

    Edited by Syun

--- removeNoiseImage.py
+++ removeNoiseImage.py
@@ -7,11 +7,12 @@ img_dir = 'E:\\mnist\\train\\2'
 for f in os.listdir(img_dir):
     img_path = os.path.join(img_dir, f)
     j = j + 1
-    if f.split('.')[len(f.split('.')) - 1] == 'zoom-10_-20_10_10.jpg' or f.split('.')[len(f.split('.')) - 1] == \
-            'trans-10_0.jpg' or f.split('.')[len(f.split('.')) - 1] == 'trans20_20.jpg':
-        os.remove(img_path)
-        i = i + 1
-        print('Noise Image')
+    for n in f.split('__'):
+        if n == 'zoom-10_-20_10_10.jpg' or n == 'trans-10_0.jpg' or n == 'trans20_20.jpg' or n == \
+                'noise0.5.jpg':
+            os.remove(img_path)
+            i = i + 1
+            print('Noise Image')
 print('Check on directory: ', img_dir)
 print('Total: ', j)
 print('Invalid file: ', i)
