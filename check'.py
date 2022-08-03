90ae576915ad63b550c6347877c230df7e15cfa3
your name <phamlong12082001@gmail.com>
2022-08-03 21:50:48 +0700

    Edited by Long

--- check'.py
+++ check'.py
@@ -12,13 +12,16 @@ img_dir0 = 'E:\\mnist\\train\\0'
 for f in os.listdir(img_dir0):
     img_path = os.path.join(img_dir0, f)
     img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+    if img is None:
+        print('Image is None')
     print('Original Dimensions : ', img.shape)
     i = i + 1
-    if img_path.split('.')[1] != 'jpg':
-        print('False')
-        j += 1
-        continue
-    elif img.shape == (28, 28) or img.shape == (28, 28, 3):
+    for n in img_path.split('.'):
+        if n != 'jpg':
+            print('False')
+            j += 1
+            continue
+    if img.shape == (28, 28) or img.shape == (28, 28, 3):
         print("True")
         k = k + 1
 print('Check on directory: ', img_dir0)
