9b0ae7526c329b105d29bb0e25c3391154904dc9
your name <phamlong12082001@gmail.com>
2022-08-05 17:11:13 +0700

    Edited by Long

--- removeFile.py
+++ removeFile.py
@@ -7,27 +7,30 @@ dim = (width, height)
 i = 0
 j = 0
 k = 0
-img_dir0 = 'E:\\mnist\\train\\1'
-for f in os.listdir(img_dir0):
-    img_path = os.path.join(img_dir0, f)
+index = input('Input the index of file: ')
+img_src = 'E:\\mnist\\train\\' + index
+nameFolder = "E:\\mnist\\train\\" + index + "_s"
+nameFile = 'number' + index + '_'
+for f in os.listdir(img_src):
+    img_path = os.path.join(img_src, f)
     img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
     i = i + 1
-    print('Original Dimensions : ', img.shape)
-    if f.split('.')[len(f.split('.')) - 1] != 'jpg' or img is None:
+    if f.split('.')[len(f.split('.')) - 1] != 'jpg' or isinstance(img, type(None)):
         # time.sleep(5)
         print('Invalid file')
         os.remove(img_path)
         j += 1
         continue
+    print('Original Dimensions : ', img.shape)
     resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
     print('Resized Dimensions : ', resized.shape)
-    if not os.path.exists("E:\\mnist\\train\\9_s"):
-        os.mkdir("E:\\mnist\\train\\9_s")
-    if not os.path.exists("E:\\mnist\\train\\9_s\\" + 'number9_' + str(i) + '.jpg'):
-        cv2.imwrite("E:\\mnist\\train\\9_s\\" + 'number9_' + str(i) + '.jpg', resized)
+    if not os.path.exists(nameFolder):
+        os.mkdir(nameFolder)
+    if not os.path.exists(nameFolder + "\\" + nameFile + str(i) + '.jpg'):
+        cv2.imwrite(nameFolder + "\\" + nameFile + str(i) + '.jpg', resized)
     cv2.imshow("Resized image", resized)
     k = k + 1
-print('Check on directory: ', img_dir0)
+print('Check on directory: ', img_src)
 print('Total: ', i)
 print('Invalid file: ', j)
 print('Valid file: ', k)
--- renameAndMoveFile.py
+++ renameAndMoveFile.py
@@ -9,9 +9,11 @@ i = 0
 j = 0
 k = 0
 index = input('Input the index of file: ')
-img_dir = 'E:\\train\\' + index
-for f in os.listdir(img_dir):
-    img_path = os.path.join(img_dir, f)
+img_src = 'E:\\train\\' + index + '_s'
+img_des = 'E:\\mnist\\train\\' + index
+nameFile = 'new_' + index
+for f in os.listdir(img_src):
+    img_path = os.path.join(img_src, f)
     img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
     i = i + 1
     if f.split('.')[len(f.split('.')) - 1] != 'jpg' or isinstance(img, type(None)):
@@ -22,8 +24,13 @@ for f in os.listdir(img_dir):
     print('Original Dimensions : ', img.shape)
     if img.shape == (28, 28) or img.shape == (28, 28, 3):
         print("True")
+        if not os.path.exists(os.path.join(img_src, nameFile + str(k) + '.jpg')):
+            os.rename(img_path, os.path.join(img_src, nameFile + str(k) + '.jpg'))
+        if os.path.isfile(os.path.join(img_src, nameFile + str(k) + '.jpg')):
+            shutil.copy(os.path.join(img_src, nameFile + str(k) + '.jpg'),
+                        os.path.join(img_des, nameFile + str(k) + '.jpg'))
         k = k + 1
-print('Check on directory: ', img_dir)
+print('Check on directory: ', img_src)
 print('Total: ', i)
 print('Invalid file: ', j)
 print('Valid file: ', k)
